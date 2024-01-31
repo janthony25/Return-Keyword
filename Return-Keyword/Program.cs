8bfcd4b040df1e588b819f66afa22460fd6992b1
janthony25 <junanthony.business@gmail.com>
2024-02-01 00:21:43 +1300

    Add project files.

---
+++ Return-Keyword/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Return
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //  return = returns data back to the place where a method is invoked
+
+            double x;
+            double y;
+            double result;
+
+            Console.WriteLine("Enter first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            result = Multiply(x, y);
+            Console.WriteLine($"{x} * {y} is: {result}");
+
+            Console.ReadKey();
+        }
+
+        static double Multiply(double x, double y)
+        {
+            /*
+            double z = x * y;
+            return z;
+            */
+
+            return x * y;
+        }
+    }
+}
